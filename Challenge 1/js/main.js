75ee956d2be0f634bcc2a87fe366663bbb402e2e
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-03-01 22:45:45 -0500

    Terminal Move Function

--- Challenge 1/js/main.js
+++
@@ -1,6 +0,0 @@
-//Please create a function that takes in two numbers. 
-//Print to the console the division of the these two numbers rounded down. 
-function divTwoRoundDown(num1, num2){
-    console.log(Math.floor(num1/num2))
-}
-divTwoRoundDown(25,3)
