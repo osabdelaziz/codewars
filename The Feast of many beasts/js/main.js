41a6a0f849874c5fb931f70e123c582d5ef854d7
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-03-07 21:01:46 -0500

    Grasshopper - Grade Book

---
+++ The Feast of many beasts/js/main.js
@@ -0,0 +1,8 @@
+function feast(beast, dish) {
+   if(beast.startsWith() === dish.startsWith() && beast.endsWith() === dish.endsWith()){
+       return true
+   } else {
+       return false
+   }
+}
+console.log(feast ('brown bear', 'bear claw'))
