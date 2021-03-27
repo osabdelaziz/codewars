1a98867abcf7ac378670e8403d3b0d53d32689ed
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-03-26 22:56:22 -0400

    Fixed: SUm without highest and lowest number

--- Sum without highest and lowest number.js
+++ Sum without highest and lowest number.js
@@ -1,5 +1,10 @@
 function sumArray(array) {
-    newArray = array.sort((a,b) => a-b)
-    return newArray.reduce((a,b) => a+b) - (newArray[0] + newArray[newArray.length - 1])
+    if(array && array.length > 1){
+      newArray = array.sort((a,b) => a-b)
+      return newArray.reduce((a,b) => a+b) - (newArray[0] + newArray[newArray.length - 1])
+    }else{
+      return 0
+    }
+    
   
   }
