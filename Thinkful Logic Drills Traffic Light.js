41612b11b743592712990ed5ba8fbd6e0c786b9b
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-04-10 22:11:23 -0400

    Thinkful Logic Drills Traffic Lights

---
+++ Thinkful Logic Drills Traffic Light.js
@@ -0,0 +1,10 @@
+function updateLight(current) {
+    if(current === 'green'){
+      return 'yellow'
+    }else if(current === 'yellow'){
+      return 'red'
+    }else if(current === 'red'){
+      return 'green'
+    }
+  
+  }
