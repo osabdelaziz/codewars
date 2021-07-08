8460a253f2b35408410e2d1dc5d010b37086e9eb
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-07-07 21:21:16 -0400

    Training JS #2

---
+++ Training JS 2.js
@@ -0,0 +1,22 @@
+let v1=50 
+let v2=100
+let v3=150
+let v4=200
+let v5=2
+let v6=250
+function equal1(){   
+  return v1 + v1
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  return v4 - v2;
+}
+function equal3(){
+  return v1 * v5;
+}
+function equal4(){
+  return v4/v5;
+}
+function equal5(){
+  return v6%v3;
+}
