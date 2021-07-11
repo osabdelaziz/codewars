98444b87b63009183780303dee34f5c49ae3622c
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-07-11 17:32:00 -0400

    Grasshopper:  Create the rooms

---
+++ Grasshopper Create the Rooms.js
@@ -0,0 +1,17 @@
+// Add rooms here
+var rooms = {
+    roomOne: {
+      name: 'bathroom',
+      description: 'Where #1 & #2 happen',
+      completed: 'yes'
+    },
+    roomTwo: {
+      name: 'bedroom',
+      description: 'Where the magic happens!',
+      completed: 'no'
+    },
+    roomThree: {
+      name: 'kitchen',
+      description: 'Culinary Closet',
+      completed: 'yes'
+    }
