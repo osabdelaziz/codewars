499b9566c2e81d5d2565c8b8b9315abf99cc888e
Omar Abdelaziz <osabdelaziz113@gmail.com>
2021-07-27 11:57:57 -0400

    Spraying Trees

---
+++ Spraying Trees.js
@@ -0,0 +1,5 @@
+function task(w, n, c) {
+    var worker = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'};
+  
+    return `It is ${w} today, ${worker[w]}, you have to work, you must spray ${n} trees and you need ${n*c} dollars to buy liquid`
+  }
